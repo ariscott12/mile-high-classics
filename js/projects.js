3405c6175733af0afb408a7603166d4b464acb3f
arsscot12 <ariscot12@gmail.com>
2015-12-10 09:22:23 +0700

    updated projects.js to load projects with ajax rather then going to new page, need to clean up code

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,8 @@
 var projects = (function($) {
     "use strict";
     // create img element for large image
-    var $imgLarge = $("<img class = \"detail-large\" data-id = \"image-large\" src = \"image\" /> ");
+    var $imgLarge = $("<img class = \"detail-large\" data-id = \"image-large\"  /> ");
+   
     var jqueryMap = {
         $project_links: $('[data-id="project-links"]'),
         $project_slides: $('[data-id="project-slides"]'),
@@ -11,12 +12,38 @@ var projects = (function($) {
         $sidebar: $('[data-id="project-sidebar"]'),
         $project_grid_link: $('[data-id=project-grid-link')
     };
-    jqueryMap.$project_slides.prepend($imgLarge);
-    var updateSrc = function($obj) {
+    //jqueryMap.$project_slides.prepend($imgLarge);
+    
+    // replace detail-large src with clicked thumbnail src, show loading gif
+    var updateSrc = function($obj, callback) {
         var largeSrc = $obj.eq(0).attr('data-image-large');
-        $imgLarge.attr('src', largeSrc);
+        var image = new Image();  
+        var imageHeight = $('.detail-large').height();
+
+        if($('.loader')) {
+            $('.loader').remove();
+        }
+
+        image.src = largeSrc;
+        $imgLarge.hide();
+
+        $('.project-slide-wrapper').prepend('<img class = "loader" />');
+        $('.loader').css({
+            height: imageHeight
+        });
+
+        image.onload = function() {
+            $('.loader').remove();
+            $imgLarge.attr('src', largeSrc);
+            $imgLarge.show();
+
+            if(typeof(callback) === 'function') {
+                callback();
+            }
+        }  
     };
-    updateSrc(jqueryMap.$first_slide.find('img'));
+    //updateSrc(jqueryMap.$first_slide.find('img'));
+    
     var animateImage = function() {
         $('.project-slide.active img').each(function(index, el) {
             $(this).delay(125 * index + 2).queue(function(next) {
@@ -50,29 +77,39 @@ var projects = (function($) {
     var loadNewProject = function() {
         // Get href from clicked element
         var link = $(this).attr('href');
-
+        var contentHeight = $('[data-id="content"]').height();
         // prepend loader
         $('[data-id="content"]').html('<img class = "loader" />');
-
+        $('.loader').css({
+            height: contentHeight
+        });
         // Scroll to content-wrapper section
         $('html,body').animate({
             scrollTop: $(".content-wrapper").offset().top
         });
 
-        $('[data-id = "content"]').load(link + ' .content-wrapper .region-content', function() {
-          
+
+        var load = $('<div>');
+        load.load(link + ' .content-wrapper .region-content', function() {
+           var content = load.html();
+           $('[data-id = "content"]').html(content);
 
             // Update large img src
             $('[data-id="project-slides"]').prepend($imgLarge);
-            updateSrc($('.project-slide').eq(0).find('img'));
-            
-            // Add CSS classes to slides to trigger animations
-            $('[data-id="project-slides"]').addClass('active')
+            updateSrc($('.project-slide').eq(0).find('img'), function() {
+                console.log('loadessd');
+            });
+          
+           // Add CSS classes to slides to trigger animations
             $('.project-slide').eq(0).addClass('active');
             $('.project-slide').eq(0).find('img').eq(0).addClass('active');
             animateImage();
             animatePagers();
-
+            $('[data-id="project-slides"]').delay(50).queue(function(next) {
+                $(this).addClass('active');
+                next();
+            });
+          
             // Add click handlers to pagers and links
             $('[data-id="project-links"]').children().click(toggleSlides);
             $('[data-id="project-pagers"]').children().click(toggleSlides);
@@ -80,37 +117,39 @@ var projects = (function($) {
         });
         return false;
     }
-    jqueryMap.$project_links.children().click(toggleSlides);
-    jqueryMap.$project_pagers.children().click(toggleSlides);
-    jqueryMap.$img_small.click(toggleLargeImage);
-    jqueryMap.$project_grid_link.find('a').click(loadNewProject);
+
+
+    // jqueryMap.$project_links.children().click(toggleSlides);
+    // jqueryMap.$project_pagers.children().click(toggleSlides);
+    // jqueryMap.$img_small.click(toggleLargeImage);
+     jqueryMap.$project_grid_link.find('a').click(loadNewProject);
     // Waypoint objects
-    if ($(".project-sidebar").length) {
-        var projectSidebar = new Waypoint({
-            element: jqueryMap.$sidebar,
-            handler: function(direction) {
-                $('.pagers a').each(function(index) {
-                    $(this).delay(125 * index).queue(function(next) {
-                        $(this).addClass('rotate');
-                        next();
-                    });
-                });
-                this.destroy();
-            },
-            offset: '75%'
-        });
-        var projectSlide = new Waypoint({
-            element: jqueryMap.$project_slides,
-            handler: function(direction) {
-                $(this.element).addClass('active');
-                jqueryMap.$first_slide.addClass('active');
-                jqueryMap.$first_slide.find('img').eq(0).addClass('active');
-                projects.animateImage();
-                this.destroy();
-            },
-            offset: '50%'
-        });
-    }
+    // if ($(".project-sidebar").length) {
+    //     var projectSidebar = new Waypoint({
+    //         element: jqueryMap.$sidebar,
+    //         handler: function(direction) {
+    //             $('.pagers a').each(function(index) {
+    //                 $(this).delay(125 * index).queue(function(next) {
+    //                     $(this).addClass('rotate');
+    //                     next();
+    //                 });
+    //             });
+    //             this.destroy();
+    //         },
+    //         offset: '75%'
+    //     });
+    //     var projectSlide = new Waypoint({
+    //         element: jqueryMap.$project_slides,
+    //         handler: function(direction) {
+    //             $(this.element).addClass('active');
+    //             jqueryMap.$first_slide.addClass('active');
+    //             jqueryMap.$first_slide.find('img').eq(0).addClass('active');
+    //             projects.animateImage();
+    //             this.destroy();
+    //         },
+    //         offset: '50%'
+    //     });
+    // }
     var projectGrid = new Waypoint({
         element: $('.project-row'),
         handler: function(direction) {
